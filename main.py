8d104e9b5c28234f5ad5da0232b42e3d452b125a
Tomasz Szpak <tomszp@gmail.com>
2024-10-11 15:21:40 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# primitive values
+integer_num = 1
+float_num = 3.14
+name = "Alice"
+is_active = True
+
+# non-primitive types
+the_list = [1, 3.4, 45]
+
+for item in the_list:
+    print(item)
+
+# dictionaries
+the_dict = {
+    "key": 456,
+}
+
+# O(n)
+# O(1)
+
+the_dict['key']
